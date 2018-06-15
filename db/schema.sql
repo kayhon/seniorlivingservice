7735c805d324d99d494898e1ff1a36b294a38fff
kayhon <kayhon@gmail.com>
2018-06-14 21:06:59 -0700

    db

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+### Schema
+DROP DATABASE IF EXISTS senior_db;
+CREATE DATABASE senior_db;
+
+USE senior_db;
+
+CREATE TABLE leads
+(
+    id int NOT NULL AUTO_INCREMENT,
+	name varchar(30) NOT NULL,
+    serv varchar(30) NOT NULL,
+	phone varchar(30) NOT NULL,
+	email varchar(30) NOT NULL,
+	zip varchar(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
