ab17d8fe456afe55e0ec6ad1fc0e9d2884129fc3
george <geomarray@gmail.com>
2018-06-15 18:23:15 -0700

    added aditional route and made our sql query

--- controllers/seniorController.js
+++ controllers/seniorController.js
@@ -3,3 +3,14 @@ var express = require("express");
 var router = express.Router();
 
 var senior = require('../models/senior.js');
+
+router.get("/", function(req, res) {
+    res.render("template");   
+});
+
+router.get("/aboutus", function(req, res) {
+    res.render("partials/aboutUs");
+});
+
+module.exports = router;
+  
--- models/senior.js
+++ models/senior.js
@@ -1,7 +1,12 @@
 var orm = require("../config/orm.js");
+
 var senior = {
-
-
+    new: function(cols, vals, cb) {
+        orm.create("leads", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    
 };
 
 module.exports = senior;
