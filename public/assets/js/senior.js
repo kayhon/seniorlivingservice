ee834f82d7933cb714d1418ec5198de0b1a2a9b6
george <geomarray@gmail.com>
2018-06-22 19:11:45 -0700

    patched the api and removed the console.log code client side

--- public/assets/js/senior.js
+++ public/assets/js/senior.js
@@ -1,32 +1,3 @@
-// // Make sure we wait to attach our handlers until the DOM is fully loaded.
-// $(document).ready(function() {
-//     $("#create-form").on("submit", function (event) {
-//         //preventDefault on a submit event.
-//         event.preventDefault();
-//         console.log('here');
-//         var newRequest = {
-//             // name: "ulysse",
-//             // serv: "assisted living",
-//             // phone: "8888888",
-//             // email: "george.gmail.com",
-//             // zip: "12345"
-//             name: $("#name").val().trim(),
-//             serv: $("#services option:selected").text(),
-//             phone: $("#phone").val().trim(),
-//             email: $("#email").val().trim(),
-//             zip: $("#zip").val().trim()
-//         };
-
-//         //send the POST request.
-//         $.ajax("/api/request", {
-//             type: "POST",
-//             data: newRequest
-//         }).then(function () {
-//             console.log("created new request");
-//             location.reload();
-//         });
-//     });
-// });
 $(document).ready(function() {
     $('#submitBtn').on("click", function () {
         event.preventDefault();
@@ -39,23 +10,23 @@ $(document).ready(function() {
             zip: $("#location").val().trim()
         };
 
-        console.log(newRequest);
+        // console.log(newRequest);
 
         // send the POST request.
         $.ajax("/api/orm", {
             type: "POST",
             data: newRequest
         }).then(function () {
-            console.log("created new request", newRequest);
+            // console.log("created new request", newRequest);
             //  location.reload();   
         });
         setQuery();
         initialize();
         callApi();
+        $("#results").empty();
     })
 });
 
-// Start of api logic
 
 var map;
 var locationInput = "";
@@ -63,8 +34,7 @@ var typeInput = "";
 var address = "";
 var service = "";
 
-// $('#targetdiv').empty();
-
+// Initializes map logic for api connection
 function initialize() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: new google.maps.LatLng(37.09024,-95.71289100000001),
@@ -76,12 +46,13 @@ function initialize() {
 function setQuery() {
     service = $("#services option:selected").text();
     locationInput = $("#location").val().trim();   
-    console.log(location);
-    console.log(service);
+    // console.log(location);
+    // console.log(service);
 };
 
+// Deletes the contents of the target div and appends a formatted api response to the dom
 function appendHTML(img , name , address, phone , rating ) {
-   $("#results").empty();
+   
     var businessCard = "";
     businessCard += "<div class='container' id='pinned_bizzcard'>"
     businessCard += "<img id='thumbnailimg' src=" + img + ">"
@@ -95,7 +66,7 @@ function appendHTML(img , name , address, phone , rating ) {
     $("#results").append(businessCard);
 };
 
-
+// API call logic
 function callApi() {
     var geocoder = new google.maps.Geocoder();
     var address = locationInput;
@@ -107,8 +78,8 @@ function callApi() {
     if (geocoder) {
         geocoder.geocode({ 'address': address }, function (results, status){
             if (status == google.maps.GeocoderStatus.OK) {
-                console.log(results[0].geometry.location.lat());
-                console.log(results[0].geometry.location.lng());
+                // console.log(results[0].geometry.location.lat());
+                // console.log(results[0].geometry.location.lng());
                 // defining the lat and lng for the next part of the api call
                 queryLatLng = new google.maps.LatLng(results[0].geometry.location.lat(),results[0].geometry.location.lng());
             }
@@ -127,12 +98,12 @@ function callApi() {
 
             function callback(results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    console.log(results);
+                    // console.log(results);
                     for (var i = 0; i < results.length; i++){
                         var place = results[i];
-                        console.log(results[i]);
+                        // console.log(results[i]);
                         idPass = results[i].place_id;
-                        console.log(idPass);
+                        // console.log(idPass);
                         // run a details call for each object coming back from the api
                         var request = {
                             placeId: idPass
@@ -145,18 +116,16 @@ function callApi() {
                       
                           function callback(resultsTwo, status) {
                             if (status == google.maps.places.PlacesServiceStatus.OK) {
-                              console.log(resultsTwo);
-                              console.log(resultsTwo.photos[0].getUrl({"maxWidth":300,"minWidth":300}));
-                              console.log(resultsTwo.name);
-                              console.log(resultsTwo.formatted_address);   
-                              console.log(resultsTwo.rating);
+                            //   console.log(resultsTwo);
+                            //   console.log(resultsTwo.photos[0].getUrl({"maxWidth":300,"minWidth":300}));
+                            //   console.log(resultsTwo.name);
+                            //   console.log(resultsTwo.formatted_address);   
+                            //   console.log(resultsTwo.rating);
                               // function appendHTML(img , name , phone , rating )
                               appendHTML(resultsTwo.photos[0].getUrl({"maxWidth":200,"minWidth":200}) , resultsTwo.name , resultsTwo.formatted_address , resultsTwo.formatted_phone_number , resultsTwo.rating)
                             }
                         }
-
-                    }
-                    
+                    }   
                 }
             }
         });
