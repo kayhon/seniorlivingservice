53af6ef3c393054d7bd8a6369e482a8321122602
kayhon <kayhon@gmail.com>
2018-08-30 16:38:03 -0700

    codeCleanup3

--- models/senior.js
+++ models/senior.js
@@ -3,9 +3,9 @@ var orm = require("../config/orm.js");
 var senior = {
     new: function(cols, vals, cb) {
         orm.create("leads", cols, vals, function(res) {
-          cb(res);
+            cb(res);
         });
-    },   
+    },
 };
 
 module.exports = senior;
